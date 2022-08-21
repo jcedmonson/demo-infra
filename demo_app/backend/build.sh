1f2ce80d511564c35614b53bf9e94dc8c90dea5b
Jacob <jedmonson1478@gmail.com>
2022-08-21 16:52:19 -0400

    adjusting flux config dir

--- demo_app/backend/build.sh
+++ demo_app/backend/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t backend:1.0 .
+docker build -t jcedmonson/demo-infra-backend:1.0 .
 # docker run -d --name backend -p 5000:5000  backend:1.0
--- demo_app/frontend/build.sh
+++ demo_app/frontend/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t frontend:1.0 .
+docker build -t jcedmonson/demo-infra-frontend:1.0 .
 # docker run -d --name frontend -p 8080:8080 frontend:1.0
